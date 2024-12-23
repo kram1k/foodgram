c5921b06d09baac9e058c938621f2d3ef579a7ea
kram1k <7685181@gmail.com>
2024-12-23 18:14:01 +0300

    fix db settings

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -61,7 +61,7 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB', default='django'),
         'USER': os.getenv('POSTGRES_USER', default='django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default=''),
