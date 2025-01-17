6f4f9854d1ddc510d2f531d8b8661950cde56160
kram1k <7685181@gmail.com>
2025-01-17 16:44:33 +0300

    Added spliting ALLOWED_HOSTS

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'key')
+SECRET_KEY = os.getenv('SECRET_KEY', 'key').split(',')
 
 DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(', ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
